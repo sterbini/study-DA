3d75a5d7ac068d5a2a645d01a099e233416941ce
Colas <colasdroin@gmail.com>
2024-05-22 17:36:52 +0200

    Progressing with mad building class

--- study_da/mad_collider.py
+++ study_da/mad_collider.py
@@ -12,7 +12,10 @@ import xmask as xm
 from cpymad.madx import Madx
 
 # Import user-defined modules
-
+from .runIII import optics_specific_tools as ost_runIII
+from .runIII_ions import optics_specific_tools as ost_runIII_ions
+from .hllhc16 import optics_specific_tools as ost_hllhc16
+from .hllhc13 import optics_specific_tools as ost_hllhc13
 
 # ==================================================================================================
 # --- Class definition
@@ -21,10 +24,35 @@ from cpymad.madx import Madx
 
 class MadCollider:
     def __init__(self, configuration: dict):
+        
         # Variables used to define the distribution
         self.links: str = configuration["links"]
 
+        # Optics specific tools
+        self.ost = self.get_ost(configuration)
+        
+        # Beam configuration
+        self.beam_configuration = configuration["optics_file"]
+        
+        # Optics
+        self.optics = configuration["optics_file"]
+            
 
+    def get_ost(self, configuration):
+        # Get the appropriate optics_specific_tools
+        match configuration["ver_hllhc_optics"]:
+            case 1.6:
+                return ost_hllhc16
+            case 1.3:
+                return ost_hllhc13
+            case 3.0:
+                if "particle_charge" in configuration["beam_config"]["lhcb1"] and configuration["beam_config"]["lhcb1"]["particle_charge"] == 82:
+                    return ost_runIII_ions
+                else:
+                    return ost_runIII
+            case _:
+                raise ValueError("No optics specific tools for this configuration")
+            
     def start_mad(self):
         # Make mad environment
         xm.make_mad_environment(links=self.links)
@@ -33,33 +61,33 @@ class MadCollider:
         mad_b1b2 = Madx(command_log="mad_collider.log")
         mad_b4 = Madx(command_log="mad_b4.log")
         return mad_b1b2, mad_b4
-
+    
     def build_collider_from_mad(self, sanity_checks=True):
 
         # Start mad
         mad_b1b2, mad_b4 = self.start_mad()
 
         # Build sequences
-        ost.build_sequence(mad_b1b2, mylhcbeam=1)
-        ost.build_sequence(mad_b4, mylhcbeam=4)
+        self.ost.build_sequence(mad_b1b2, mylhcbeam=1, beam_config = self.beam_configuration)
+        self.ost.build_sequence(mad_b4, mylhcbeam=4, beam_config = self.beam_configuration)
 
         # Apply optics (only for b1b2, b4 will be generated from b1b2)
-        ost.apply_optics(mad_b1b2, optics_file=self.configuration["optics_file"])
+        self.ost.apply_optics(mad_b1b2, optics_file=self.optics)
 
         if sanity_checks:
             mad_b1b2.use(sequence="lhcb1")
             mad_b1b2.twiss()
-            ost.check_madx_lattices(mad_b1b2)
+            self.ost.check_madx_lattices(mad_b1b2)
             mad_b1b2.use(sequence="lhcb2")
             mad_b1b2.twiss()
-            ost.check_madx_lattices(mad_b1b2)
+            self.ost.check_madx_lattices(mad_b1b2)
 
         # Apply optics (only for b4, just for check)
-        ost.apply_optics(mad_b4, optics_file=self.configuration["optics_file"])
+        self.ost.apply_optics(mad_b4, optics_file=self.optics)
         if sanity_checks:
             mad_b4.use(sequence="lhcb2")
             mad_b4.twiss()
-            ost.check_madx_lattices(mad_b1b2)
+            self.ost.check_madx_lattices(mad_b1b2)
 
         # Build xsuite collider
         collider = xlhc.build_xsuite_collider(
@@ -84,7 +112,8 @@ class MadCollider:
 
 
     def activate_RF_and_twiss(collider, self.configuration, sanity_checks=True):
-        # Define a RF system (values are not so immportant as they're defined later)
+        # Define a RF system
+        # ! LOAD PHASING FROM CONFIG OTHERWISE IT WON'T WORK
         print("--- Now Computing Twiss assuming:")
         if self.configuration["ver_hllhc_optics"] == 1.6:
             dic_rf = {"vrf400": 16.0, "lagrf400.b1": 0.5, "lagrf400.b2": 0.5}
