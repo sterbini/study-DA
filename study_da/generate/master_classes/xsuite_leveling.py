fde50ddb83e87e7a4bf34328bb430e6402367d0d
Colas Droin <colasdroin@gmail.com>
2024-10-07 14:17:12 +0200

    Added docstrings for xsuite_leveling

--- study_da/generate/master_classes/xsuite_leveling.py
+++ study_da/generate/master_classes/xsuite_leveling.py
@@ -15,17 +15,48 @@ from scipy.optimize import minimize_scalar
 # ==================================================================================================
 # --- Functions used for leveling
 # ==================================================================================================
-def compute_PU(luminosity, num_colliding_bunches, T_rev0, cross_section):
+def compute_PU(luminosity: float, num_colliding_bunches: int, T_rev0: float, cross_section: float):
+    """
+    Compute the Pile-Up (PU) value.
+
+    Args:
+        luminosity (float): The luminosity of the collider.
+        num_colliding_bunches (int): The number of colliding bunches.
+        T_rev0 (float): The revolution time of the collider.
+        cross_section (float): The cross-section value.
+
+    Returns:
+        float: The computed Pile-Up (PU) value.
+    """
     return luminosity / num_colliding_bunches * cross_section * T_rev0
 
 
 def luminosity_leveling_ip1_5(
-    collider,
-    config_lumi_leveling_ip1_5,
-    config_beambeam,
-    crab=False,
-    cross_section=81e-27,
+    collider: xt.Multiline,
+    config_lumi_leveling_ip1_5: dict,
+    config_beambeam=dict,
+    crab: bool = False,
+    cross_section: float = 81e-27,
 ):
+    """
+    Perform luminosity leveling for interaction points IP1 and IP5.
+
+    Args:
+        collider (dict): Dictionary containing collider objects for beams 'lhcb1' and 'lhcb2'.
+        config_lumi_leveling_ip1_5 (dict): Configuration dictionary for luminosity leveling at IP1
+            and IP5. Must contain 'num_colliding_bunches' and 'constraints' with 'max_intensity'
+            and 'max_PU'.
+        config_beambeam (dict): Configuration dictionary for beam-beam parameters. Must contain
+            'nemitt_x', 'nemitt_y', and 'sigma_z'.
+        crab (bool): Flag to indicate if crab cavities are used. Default to False.
+        cross_section (float): Cross-section value in square meters. Default to 81e-27.
+
+    Returns:
+        float: Optimized bunch intensity for leveling in IP1 and IP5.
+
+    Raises:
+        Warning: If the optimization for leveling in IP1/5 fails, a warning is logged.
+    """
     # Get Twiss
     twiss_b1 = collider["lhcb1"].twiss()
     twiss_b2 = collider["lhcb2"].twiss()
@@ -36,7 +67,7 @@ def luminosity_leveling_ip1_5(
     # Get max intensity in IP1/5
     max_intensity_IP1_5 = float(config_lumi_leveling_ip1_5["constraints"]["max_intensity"])
 
-    def compute_lumi(bunch_intensity):
+    def _compute_lumi(bunch_intensity):
         luminosity = xt.lumi.luminosity_from_twiss(  # type: ignore
             n_colliding_bunches=n_colliding_IP1_5,
             num_particles_per_bunch=bunch_intensity,
@@ -51,7 +82,7 @@ def luminosity_leveling_ip1_5(
         return luminosity
 
     def f(bunch_intensity):
-        luminosity = compute_lumi(bunch_intensity)
+        luminosity = _compute_lumi(bunch_intensity)
 
         max_PU_IP_1_5 = config_lumi_leveling_ip1_5["constraints"]["max_PU"]
 
