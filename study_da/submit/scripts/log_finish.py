547bcdeb4b5daa49e30d7c160b870022c5528f6c
Colas Droin <colasdroin@gmail.com>
2024-10-14 17:24:57 +0200

    Update version + try long sleep time not to corrupt tree

--- study_da/submit/scripts/log_finish.py
+++ study_da/submit/scripts/log_finish.py
@@ -22,7 +22,7 @@ tree_path = sys.argv[1]
 l_keys = sys.argv[2:]
 
 # Define lock
-lock = SoftFileLock(f"{tree_path}.lock", timeout=1000)
+lock = SoftFileLock(f"{tree_path}.lock", timeout=3000)
 
 # Update tag
 with lock:
@@ -35,5 +35,5 @@ with lock:
     # Write the tree
     write_dic_to_path(tree, tree_path)
 
-    # Wait 0.2 seconds to ensure the file is properly written when the lock is released
-    time.sleep(0.1)
+    # Wait 0.5 seconds to ensure the file is properly written when the lock is released
+    time.sleep(0.5)
