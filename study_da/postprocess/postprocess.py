50ddb6eaff17ffa8960c08e741c03f8e159f0337
Colas Droin <colasdroin@gmail.com>
2024-10-14 10:13:40 +0200

    First working version postprocessing

--- study_da/postprocess/postprocess.py
+++ study_da/postprocess/postprocess.py
@@ -19,18 +19,26 @@ from study_da.utils.configuration import load_dic_from_path
 # ==================================================================================================
 # --- Functions to browse simulations folder and extract relevant observables
 # ==================================================================================================
-def get_particles_data(dic_all_jobs, name_output="output_particles.parquet"):
+def get_particles_data(
+    dic_all_jobs,
+    absolute_path_study,
+    generation_of_interest=2,
+    name_output="output_particles.parquet",
+):
     l_df_output = []
-    for job, path_job in dic_all_jobs.items():
+    for relative_path_job, dic_job in dic_all_jobs.items():
+        if dic_job["gen"] != generation_of_interest:
+            continue
+        absolute_path_job = f"{absolute_path_study}/{relative_path_job}"
+        absolute_folder_job = os.path.dirname(absolute_path_job)
         try:
-            df_output = pd.read_parquet(f"{path_job}/{name_output}")
+            df_output = pd.read_parquet(f"{absolute_folder_job}/{name_output}")
         except Exception as e:
-            print(e)
-            logging.warning(f"{job } does not have {name_output}")
+            logging.warning(e)
             continue
 
         # Register path of the job
-        df_output["name base collider"] = job
+        df_output["name base collider"] = relative_path_job
 
         # Add to the list
         l_df_output.append(df_output)
@@ -38,14 +46,21 @@ def get_particles_data(dic_all_jobs, name_output="output_particles.parquet"):
     return l_df_output
 
 
-def add_parameters_from_config(l_df_output, dic_parameters_of_interest):
+def add_parameters_from_config(
+    l_df_output, dic_parameters_of_interest, default_path_template_parameters=False
+):
     for df_output in l_df_output:
         # Get generation configurations as dictionnaries for parameter assignation
-        dic_configuration = df_output.attrs["configuration"]["config_collider"]
+        dic_configuration = df_output.attrs["configuration"]
 
         # Select simulations parameters of interest
         for name_param, l_path_param in dic_parameters_of_interest.items():
-            df_output[name_param] = nested_get(dic_configuration, l_path_param)
+            try:
+                df_output[name_param] = nested_get(dic_configuration, l_path_param)
+            except KeyError:
+                # Only be verbose if the dic_parameters_of_interest has not been provided by the user
+                if not default_path_template_parameters:
+                    logging.warning(f"Parameter {name_param} not found in the configuration file")
 
     return l_df_output
 
@@ -56,8 +71,12 @@ def merge_and_group_by_parameters_of_interest(
     only_keep_lost_particles=True,
     l_parameters_to_keep=None,
 ):
+    # Merge all dataframes
+    df_all_sim = pd.concat(df_all_sim)
+
     # Handle mutable default arguments
     if l_parameters_to_keep is None:
+        logging.info("No list of parameters to keep provided, keeping all available parameters")
         l_parameters_to_keep = list(df_all_sim.columns)
 
     if only_keep_lost_particles:
@@ -81,7 +100,10 @@ def merge_and_group_by_parameters_of_interest(
 def aggregate_output_data(
     path_tree: str,
     l_group_by_parameters: list[str],
+    generation_of_interest=2,
+    name_output="output_particles.parquet",
     write_output=True,
+    path_output=None,
     only_keep_lost_particles=True,
     dic_parameters_of_interest=None,
     l_parameters_to_keep: Optional[list[str]] = None,
@@ -89,18 +111,24 @@ def aggregate_output_data(
     path_template_parameters=None,
 ):
     # Start of the script
-    logging.warning("Analysis of output simulation files started")
+    logging.info("Analysis of output simulation files started")
 
     # Load the tree
     dic_tree, _ = load_dic_from_path(path_tree)
 
+    # Get absolute path of the study
+    absolute_path_study = dic_tree["absolute_path"]
+
     # Get all jobs
     dic_all_jobs = ConfigJobs(dic_tree).find_all_jobs()
 
-    # Get particles data
-    l_df_sim = get_particles_data(dic_all_jobs)
+    # # Get particles data
+    l_df_sim = get_particles_data(
+        dic_all_jobs, absolute_path_study, generation_of_interest, name_output
+    )
 
     # Define parameters of interest
+    default_path_template_parameters = False
     if dic_parameters_of_interest is None:
         if path_template_parameters is not None:
             # Load dic_parameters_of_interest from the template configs
@@ -110,22 +138,19 @@ def aggregate_output_data(
                 raise ValueError(
                     "No template configuration file provided for the parameters of interest"
                 )
-
             # Load dic_parameters_of_interest from the template configs
             logging.info("Loading parameters of interest from the template configuration file")
             path_template_parameters = (
                 f"{os.path.dirname(inspect.getfile(aggregate_output_data))}"
                 f"/configs/{name_template_parameters}"
             )
+            default_path_template_parameters = True
         dic_parameters_of_interest, _ = load_dic_from_path(path_template_parameters)
 
-    # Reorganize data
-    l_df_output = add_parameters_from_config(l_df_sim, dic_parameters_of_interest)
-
-    # Keep only the parameters of interest (all by default)
-    if l_parameters_to_keep is None:
-        logging.info("No list of parameters to keep provided, keeping all parameters")
-        l_parameters_to_keep = list(dic_parameters_of_interest.keys())
+    # Add parameters from config to the output data
+    l_df_output = add_parameters_from_config(
+        l_df_sim, dic_parameters_of_interest, default_path_template_parameters
+    )
 
     # Merge and group by parameters of interest
     df_final = merge_and_group_by_parameters_of_interest(
@@ -134,8 +159,11 @@ def aggregate_output_data(
 
     # Save data
     if write_output:
-        path_study = "/".join(path_tree.split("/")[:-1])
-        df_final.to_parquet(f"{path_study}/da.parquet")
+        if path_output is None:
+            path_output = f"{absolute_path_study}/da.parquet"
+        df_final.to_parquet(path_output)
+    elif path_output is not None:
+        logging.warning("Output path provided but write_output set to False, no output saved")
 
     logging.info("Final dataframe for current set of simulations: ", df_final)
     return df_final
